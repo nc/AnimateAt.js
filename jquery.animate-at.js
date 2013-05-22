ef4f506a9336115a918908f5c809955a157200ec
nc <namit.chadha@gmail.com>
2013-05-22 09:23:03 +0100

    pages

---
+++ jquery.animate-at.js
@@ -0,0 +1,38 @@
+/*global jQuery */
+/*!
+* AnimateAt.js 0.1.0
+*
+* Copyright (c) 2013 Namit Chadha http://namit.me
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* Date: Thu May 21 23:21:00 2013 0000
+*/
+
+(function ( $ ) {
+ 
+    $.animateAt = function() {
+      $("[data-animate-from-hidden]").hide();
+
+      var toAnimate;
+
+      // find elements to animate
+      toAnimate = $("[data-animate-at]");
+
+      toAnimate.each(function() {
+        var animateAt, el;
+        el = $(this);
+        animateAt = parseInt(el.attr("data-animate-at"));
+        return setTimeout((function() {
+          return el.addClass("animated").show();
+        }), animateAt);
+      });
+
+      return this;
+    };
+ 
+}( jQuery ));
